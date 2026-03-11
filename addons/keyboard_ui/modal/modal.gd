4e88b39be2d584486035c6986cf1ae6f3edede6e
Joozey <jostievanegmond@gmail.com>
2026-03-12 00:37:14 +0100

    Dont make a sound when initially visible. Only make sound when calling open or close. Store ids instead of nodes to better deal with freed or otherwise indisposed/invalid nodes.

--- addons/keyboard_ui/modal/modal.gd
+++ addons/keyboard_ui/modal/modal.gd
@@ -13,7 +13,7 @@ signal dismiss(triggering_event: InputEvent)
 
 
 var _glass: ColorRect
-var _trapped_nodes: Dictionary = {} # Control -> original FocusMode
+var _trapped_nodes: Dictionary = {}
 
 
 func _ready() -> void:
@@ -22,7 +22,8 @@ func _ready() -> void:
 	add_child(_glass, false, Node.INTERNAL_MODE_FRONT)
 	_update_modal_glass()
 	if not Engine.is_editor_hint():
-		_on_visibility_changed()
+		if visible:
+			_trap_focus()
 		visibility_changed.connect(_on_visibility_changed)
 
 
@@ -40,28 +41,29 @@ func _exit_tree() -> void:
 func _open() -> void:
 	_trap_focus()
 
-	if !Engine.is_editor_hint():
-		UIAudio.notify(self, UIAudio.MODAL_OPEN)
+	UIAudio.notify(self, UIAudio.MODAL_OPEN)
 
 
 func _close() -> void:
 	_release_focus()
 
-	if !Engine.is_editor_hint():
-		UIAudio.notify(self, UIAudio.MODAL_CLOSE)
+	UIAudio.notify(self, UIAudio.MODAL_CLOSE)
 
 
 func _trap_focus() -> void:
 	_trapped_nodes.clear()
 	_collect_focusable(get_tree().root)
-	for node: Control in _trapped_nodes:
-		node.focus_mode = Control.FOCUS_NONE
+	for id in _trapped_nodes:
+		var node = instance_from_id(id)
+		if is_instance_valid(node):
+			node.focus_mode = Control.FOCUS_NONE
 
 
 func _release_focus() -> void:
-	for node: Control in _trapped_nodes:
+	for id in _trapped_nodes:
+		var node = instance_from_id(id)
 		if is_instance_valid(node):
-			node.focus_mode = _trapped_nodes[node]
+			node.focus_mode = _trapped_nodes[id]
 	_trapped_nodes.clear()
 
 
@@ -69,7 +71,7 @@ func _collect_focusable(node: Node) -> bool:
 	if node == self:
 		return true
 	if node is Control and node.focus_mode != Control.FOCUS_NONE:
-		_trapped_nodes[node] = node.focus_mode
+		_trapped_nodes[node.get_instance_id()] = node.focus_mode
 	for child in node.get_children():
 		if _collect_focusable(child):
 			return true
@@ -84,6 +86,5 @@ func _update_modal_glass():
 func _unhandled_input(event: InputEvent) -> void:
 	super._unhandled_input(event)
 
-	if event.is_action_pressed("ui_cancel"):
+	if visible and event.is_action_pressed("ui_cancel"):
 		dismiss.emit(event)
-		get_viewport().set_input_as_handled()
